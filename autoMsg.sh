8761b0f4d30fbe62e9b9653db7ce383af23abb63
jacob fraizer <jacob33c@gmail.com>
2020-08-11 20:05:48 -0700

    Initial commit

---
+++ autoMsg.sh
@@ -0,0 +1,13 @@
+targetBuddyPhone='‭+‭15592137653‬'
+
+while read targetMessage; do
+echo $targetMessage
+
+osascript <<EOF
+tell application "Messages"
+    set targetService to 1st service whose service type = iMessage
+    set targetBuddy to buddy "$targetBuddyPhone" of targetService
+    send "$targetMessage" to targetBuddy
+end tell
+EOF
+done < text.txt
